caa874270e8533c60420182143bc533bd853a958
Ian Davis <18375+iand@users.noreply.github.com>
2023-07-05 12:28:07 +0100

    Generic kadkey

--- key/key.go
+++ key/key.go
@@ -1,69 +1,62 @@
 package key
 
 import (
+	"bytes"
 	"encoding/hex"
 	"math"
+	"unsafe"
 )
 
-type KadKey []byte
-
-func (k KadKey) Size() int {
-	return len(k)
+type KadKey[T any] struct {
+	bytes []byte
 }
 
-func (k KadKey) Hex() string {
-	return hex.EncodeToString(k[:])
+type KadKey256 = KadKey[[256]byte]
+
+func New[T any](data []byte) KadKey[T] {
+	var v T
+	if uintptr(len(data)) != unsafe.Sizeof(v) {
+		panic("invalid data lenght for key")
+	}
+	return KadKey[T]{bytes: data}
 }
 
-func (k KadKey) String() string {
+func (k KadKey[T]) Size() int {
+	return len(k.bytes)
+}
+
+func (k KadKey[T]) Hex() string {
+	return hex.EncodeToString(k.bytes[:])
+}
+
+func (k KadKey[T]) String() string {
 	return k.Hex()
 }
 
-func (a KadKey) Xor(b KadKey) (KadKey, error) {
-	if a.Size() != b.Size() {
-		return nil, ErrInvalidKey(a.Size())
-	}
-
+func (a KadKey[T]) Xor(b KadKey[T]) KadKey[T] {
 	xored := make([]byte, a.Size())
 	for i := 0; i < a.Size(); i++ {
-		xored[i] = a[i] ^ b[i]
+		xored[i] = a.bytes[i] ^ b.bytes[i]
 	}
-	return xored, nil
+	return KadKey[T]{bytes: xored}
 }
 
-func (a KadKey) CommonPrefixLength(b KadKey) (int, error) {
-	if a.Size() != b.Size() {
-		return 0, ErrInvalidKey(a.Size())
-	}
-
+func (a KadKey[T]) CommonPrefixLength(b KadKey[T]) int {
 	var xored byte
 	for i := 0; i < a.Size(); i++ {
-		xored = a[i] ^ b[i]
+		xored = a.bytes[i] ^ b.bytes[i]
 		if xored != 0 {
-			return i*8 + 7 - int(math.Log2(float64(xored))), nil
+			return i*8 + 7 - int(math.Log2(float64(xored)))
 		}
 	}
-	return 8 * a.Size(), nil
+	return 8 * a.Size()
 }
 
 // Compare returns -1 if a < b, 0 if a == b, and 1 if a > b
-func (a KadKey) Compare(b KadKey) (int8, error) {
-	if a.Size() != b.Size() {
-		return 2, ErrInvalidKey(a.Size())
-	}
-
-	for i := 0; i < a.Size(); i++ {
-		if a[i] < b[i] {
-			return -1, nil
-		}
-		if a[i] > b[i] {
-			return 1, nil
-		}
-	}
-	return 0, nil
+func (a KadKey[T]) Compare(b KadKey[T]) int {
+	return bytes.Compare(a.bytes, b.bytes)
 }
 
-func (a KadKey) Equal(b KadKey) (bool, error) {
-	cmp, err := a.Compare(b)
-	return cmp == 0, err
+func (a KadKey[T]) Equal(b KadKey[T]) bool {
+	return a.Compare(b) == 0
 }
