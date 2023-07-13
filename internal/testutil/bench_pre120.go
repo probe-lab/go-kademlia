e262f292a80a1967c7fbef2e34804ad71b5c2c39
Ian Davis <18375+iand@users.noreply.github.com>
2023-07-13 10:18:38 +0100

    Fix method signature of ReportTimePerItemMetric on pre 1.20 versions of Go

--- internal/testutil/bench_pre120.go
+++ internal/testutil/bench_pre120.go
@@ -5,6 +5,6 @@ package testutil
 import "testing"
 
 // ReportTimePerItemMetric is a no-op on versions of Go before 1.20
-func ReportTimePerItemMetric(b *testing.B, n int) {
+func ReportTimePerItemMetric(b *testing.B, n int, name string) {
 	// no-op
 }
