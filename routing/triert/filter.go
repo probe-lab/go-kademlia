3ad3ed6de38e5e1071d05f5d8f23540319b07f1d
Dennis Trautwein <git@dtrautwein.eu>
2023-08-08 17:18:21 +0200

    Let triert comply to new routing table interface

--- routing/triert/filter.go
+++ routing/triert/filter.go
@@ -4,10 +4,10 @@ import "github.com/plprobelab/go-kademlia/kad"
 
 // KeyFilterFunc is a function that is applied before a key is added to the table.
 // Return false to prevent the key from being added.
-type KeyFilterFunc[K kad.Key[K]] func(rt *TrieRT[K], kk K) bool
+type KeyFilterFunc[K kad.Key[K], N kad.NodeID[K]] func(rt *TrieRT[K, N], kk K) bool
 
 // BucketLimit20 is a filter function that limits the occupancy of buckets in the table to 20 keys.
-func BucketLimit20[K kad.Key[K]](rt *TrieRT[K], kk K) bool {
+func BucketLimit20[K kad.Key[K], N kad.NodeID[K]](rt *TrieRT[K, N], kk K) bool {
 	cpl := rt.Cpl(kk)
 	return rt.CplSize(cpl) < 20
 }
